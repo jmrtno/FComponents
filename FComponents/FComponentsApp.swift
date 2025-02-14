aa467c708d63a25d5ad798f3fe6b19c6968c6966
jmrtno <javiermartino96@gmail.com>
2025-02-14 14:48:19 +0100

    Initial Commit

---
+++ FComponents/FComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FComponentsApp.swift
+//  FComponents
+//
+//  Created by Javier Martin on 14/2/25.
+//
+
+import SwiftUI
+
+@main
+struct FComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
